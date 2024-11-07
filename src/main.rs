11761dbdd2dffede5686eba9d4768f6d4ce9267d
masterchange <2042313869@qq.com>
2024-11-07 13:36:47 +0800

    demo

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9090").unwrap();
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+        // handle connection
+        println!("New connection: {}", stream.peer_addr().unwrap());
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("Response sent!");
+}
